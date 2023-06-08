3650924447fc0c97b4bb3532c875736f98e6588d
anazalu <109597957+anazalu@users.noreply.github.com>
2023-06-08 12:59:29 +0300

    Update views.py

--- web/orderapi/views.py
+++ web/orderapi/views.py
@@ -15,6 +15,19 @@ def customer_list(request):
         customer_id = request.POST.get('customer_id')
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
-        customer = Customer.objects.create(title=title, author=author, publication_date=publication_date)
+        customer = Customer.objects.create(customer_id=customer_id, last_name=last_name, email=email)
         return JsonResponse({'message': 'Customer created successfully.'})
       
+@csrf_exempt
+def product_list(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        customer_data = [{'product_id': product.product_id, 'product_name': product.product_name, 'price': product.price} for product in products]
+        return JsonResponse({'products': product_data})
+    elif request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        product_name = request.POST.get('product_name')
+        price = request.POST.get('price')
+        product = Product.objects.create(title=title, author=author, publication_date=publication_date)
+        return JsonResponse({'message': 'Product created successfully.'})
+    
