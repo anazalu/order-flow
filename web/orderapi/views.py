771e8acae5b05a6cd1b6ae12c1e46948dd52954e
anazalu <109597957+anazalu@users.noreply.github.com>
2023-06-08 13:08:49 +0300

    Update views.py

--- web/orderapi/views.py
+++ web/orderapi/views.py
@@ -22,12 +22,25 @@ def customer_list(request):
 def product_list(request):
     if request.method == 'GET':
         products = Product.objects.all()
-        customer_data = [{'product_id': product.product_id, 'product_name': product.product_name, 'price': product.price} for product in products]
+        product_data = [{'product_id': product.product_id, 'product_name': product.product_name, 'price': product.price} for product in products]
         return JsonResponse({'products': product_data})
     elif request.method == 'POST':
         product_id = request.POST.get('product_id')
         product_name = request.POST.get('product_name')
         price = request.POST.get('price')
-        product = Product.objects.create(title=title, author=author, publication_date=publication_date)
+        product = Product.objects.create(product_id=product_id, product_name=product_name, price=price)
         return JsonResponse({'message': 'Product created successfully.'})
+    
+@csrf_exempt
+def order_list(request):
+    if request.method == 'GET':
+        orders = Order.objects.all()
+        order_data = [{'order_id': order.order_id, 'customer_id': order.customer_id, 'stage': order.stage} for order in orders]
+        return JsonResponse({'orders': order_data})
+    elif request.method == 'POST':
+        order_id = request.POST.get('order_id')
+        customer_id = request.POST.get('customer_id')
+        stage = request.POST.get('stage')
+        order = Order.objects.create(order_id=order_id, customer_id=customer_id, stage=stage)
+        return JsonResponse({'message': 'Order created successfully.'})
     
