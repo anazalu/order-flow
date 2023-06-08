f221684a2744c811b45849cf967f7ccef173dd2c
anazalu <109597957+anazalu@users.noreply.github.com>
2023-06-08 12:31:53 +0300

    Update views.py

--- web/orderapi/views.py
+++ web/orderapi/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Customer, 
+
+@csrf_exempt
+def customer_list(request):
+    if request.method == 'GET':
+        customers = Customer.objects.all()
+        book_data = [{'title': book.title, 'author': book.author, 'publication_date': book.publication_date} for customer in customers]
+        return JsonResponse({'customers': customer_data})
+    elif request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        publication_date = request.POST.get('publication_date')
+        book = Book.objects.create(title=title, author=author, publication_date=publication_date)
+        return JsonResponse({'message': 'Book created successfully.'})
+      
