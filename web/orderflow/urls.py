3b80b5df90e74bdcdaf94a626eaf6bed2d859383
anazalu <109597957+anazalu@users.noreply.github.com>
2023-06-08 12:35:19 +0300

    Update urls.py

--- web/orderflow/urls.py
+++ web/orderflow/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import customer_list
+
+urlpatterns = [
+    path('customers/', customer_list, name='customer-list'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
