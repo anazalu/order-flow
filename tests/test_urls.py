36a94b6c62059467d7bede118c721611d12828a9
Your Name <you@example.com>
2023-06-11 17:19:59 +0300

    set up GitHub Actions WIP

---
+++ tests/test_urls.py
@@ -0,0 +1,8 @@
+from django.urls import reverse, resolve
+from django.urls import path
+
+class TestUrls: 
+# here, you are checking if the path's view name is content
+    def test_post_content_url(self):  
+        path = reverse('content', kwargs={'pk':1})  
+        assert resolve(path).view_name == "content"  # here you are checking if the path's view name is content
