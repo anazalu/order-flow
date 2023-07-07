3ff6bb40fa2f60fca0d9f2c40764b429ef0bcaef
Your Name <you@example.com>
2023-07-07 18:15:34 +0300

    tune the CI workflow

--- web/orderflow/settings.py
+++ web/orderflow/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': os.environ.get('POSTGRES_HOST'), # 'db',
         'PORT': 5432,
     }
 }
