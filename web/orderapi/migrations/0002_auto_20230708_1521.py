c689f03bca100e664e19fe5b5c5ecdafe51d7e0b
Your Name <you@example.com>
2023-07-08 18:33:58 +0300

    tune Cypress

--- web/orderapi/migrations/0002_auto_20230708_1521.py
+++ web/orderapi/migrations/0002_auto_20230708_1521.py
@@ -62,7 +62,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='order',
             name='id',
-            field=models.AutoField(default=django.utils.timezone.now, primary_key=True, serialize=False),
+            field=models.AutoField(default=1, primary_key=True, serialize=False),
             preserve_default=False,
         ),
         migrations.AddField(
